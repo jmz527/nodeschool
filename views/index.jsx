00c2077b97cbd559e78ed97c3f167a7331329d90
James Rutledge <jarutledge2@gmail.com>
2016-01-21 20:48:42 -0500

    state passed, ugh...

--- views/index.jsx
+++ views/index.jsx
@@ -89,9 +89,25 @@ class TodoList extends React.Component {
 =======
 class Todo extends React.Component {
 	// Write code here
+	constructor(props) {
+		super(props);
+		this.state = {checked: false};
+	}
+
+	handleChange(e) {
+		this.setState({checked: e.target.checked});
+	}
+
+	// getInitialState() {
+	// 	this.state = {checked: true};
+	// }
+
 	render() {
 		return (
 			<tr>
+				<td style={{border: "1px solid black"}}>
+					<input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+				</td>
 				<td style={{border: "1px solid black"}}>{this.props.title}</td>
 				<td style={{border: "1px solid black"}}>{this.props.children}</td>
 			</tr>
