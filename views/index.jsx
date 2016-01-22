a0e03f339d65f7e826b7bdebb937f7b7ca6476a9
James Rutledge <jarutledge2@gmail.com>
2016-01-21 20:06:15 -0500

    props passed

--- views/index.jsx
+++ views/index.jsx
@@ -72,13 +72,33 @@ class TodoList extends React.Component {
   render() {
   	return (
 	  <div className="todoList">
-	  	I am a TodoList.
+	  	<table style={{border: "2px solid black"}}>
+	  		<tbody>
+	  		<Todo title="Shopping">Milk</Todo>
+	  		<Todo title="Hair cut">13:00</Todo>
+	  		</tbody>
+	  	</table>
 	  </div>
 	);
   }
 }
 
+<<<<<<< HEAD
 >>>>>>> initial commit, learnyoureact started
+=======
+class Todo extends React.Component {
+	// Write code here
+	render() {
+		return (
+			<tr>
+				<td style={{border: "1px solid black"}}>{this.props.title}</td>
+				<td style={{border: "1px solid black"}}>{this.props.children}</td>
+			</tr>
+		);
+	}
+}
+
+>>>>>>> props passed
 class TodoForm extends React.Component {
   // Write code here
   render() {
