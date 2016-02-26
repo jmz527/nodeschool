11b6fdaa173c9d6717fa08614c64df620cce7e3b
James Rutledge <jarutledge2@gmail.com>
2016-02-26 02:43:37 -0500

    ugh...this one's rough

---
+++ count-to-6/tagged-template-strings.js
@@ -0,0 +1,17 @@
+// console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+// function html(['<b>', ' says</b>: "', '"'], username, comment) {
+// 	username = username.replace("'", "&apos;");
+// 	username = username.replace('"', "&quot;");
+// 	username = username.replace('<', "&lt;");
+// 	username = username.replace('>', "&gt;");
+// 	username = username.replace('&', "&amp;");
+// 	comment = comment.replace("'", "&apos;");
+// 	comment = comment.replace('"', "&quot;");
+// 	comment = comment.replace('<', "&lt;");
+// 	comment = comment.replace('>', "&gt;");
+// 	comment = comment.replace('&', "&amp;");
+// }
+
+console.log(process.argv[2]);
+console.log(process.argv[3]);
