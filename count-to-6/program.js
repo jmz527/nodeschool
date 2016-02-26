c98c3160fb2eeb090f7a9fa2bd138b8631350990
James Rutledge <jarutledge2@gmail.com>
2016-02-26 01:43:29 -0500

    spread

--- count-to-6/program.js
+++ count-to-6/program.js
@@ -6,14 +6,3 @@ module.exports = function average(...args) {
 	result = (result/l);
 	return result
 };
-
-
-// var args = process.argv;
-// // console.log(args);
-// // console.log(args.splice(2, (args.length - 2)))
-// var numbers = args.splice(2, (args.length - 2));
-
-// // var numbers = [1, 1, 2, 3, 5, 8];
-// var min = Math.min(...numbers);
-
-// console.log(`The minimum of [${numbers}] is ${min}`);
