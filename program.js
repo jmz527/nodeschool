255b715556b02f676f74ca6f450a6c7be0b1f3bd
James Rutledge <jarutledge2@gmail.com>
2016-02-26 00:57:32 -0500

    ...

--- program.js
+++ program.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> isomorphic, check
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var DOM = React.DOM;
@@ -12,8 +15,11 @@ var script = DOM.script;
 var browserify = require('browserify');
 var babelify = require('babelify');
 
+<<<<<<< HEAD
 =======
 >>>>>>> initial commit, learnyoureact started
+=======
+>>>>>>> isomorphic, check
 app.set('port', (process.argv[2] || 3000));
 app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
@@ -23,6 +29,7 @@ require('babel/register')({
     ignore: false
 });
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 var TodoBox = require('./views/index.jsx');
@@ -63,11 +70,45 @@ app.use('/', function(req, res) {
 >>>>>>> initial commit, learnyoureact started
 =======
 // write below
+=======
+var TodoBox = require('./views/index.jsx');
+>>>>>>> isomorphic, check
 var data = [{'title': 'Shopping', 'detail': process.argv[3] }, {'title': 'Hair cut', 'detail': process.argv[4] }]
 
+app.use('/bundle.js', function(req, res) {
+    res.setHeader('content-type', 'application/javascript');
+
+    browserify({ debug: true })
+        .transform(babelify.configure({
+            presets: ["react", "es2015"]
+        }))
+        .require("./app.js", { entry: true })
+        .bundle()
+        .pipe(res);
+});
+
 app.use('/', function(req, res) {
+<<<<<<< HEAD
   res.render('index', {data: data});
 >>>>>>> properties are from the server
+=======
+    var initialData = JSON.stringify(data);
+    var markup = ReactDOMServer.renderToString(React.createElement(TodoBox, {data: data}));
+
+    res.setHeader('Content-Type', 'text/html');
+
+    var html = ReactDOMServer.renderToStaticMarkup(body(null,
+        div({id: 'app', dangerouslySetInnerHTML: {__html: markup}}),
+        script({
+            id: 'initial-data',
+            type: 'text/plain',
+            'data-json': initialData
+        }),
+        script({src: '/bundle.js'})
+    ));
+
+    res.end(html);
+>>>>>>> isomorphic, check
 });
 
 app.listen(app.get('port'), function() {});
