c3435b6a121f57667f4ae77745042cd462a3991a
James Rutledge <jarutledge2@gmail.com>
2016-02-26 01:39:16 -0500

    arrow functions

--- count-to-6/program.js
+++ count-to-6/program.js
@@ -8,14 +8,7 @@ module.exports = function average(...args) {
 };
 
 
-// var inputs = process.argv.slice(2);
-// // console.log(inputs);
-// // console.log(inputs.concat());
-// // console.log(inputs.map(str => str.charAt(0)));
-// // console.log(inputs.map(str => str.charAt(0)).reduce((arry, str) => arry.concat(str)));
 
-// var result = inputs.map(str => str[0]).reduce((arry, str) => arry + str);
-// console.log(`[${inputs}] becomes "${result}"`);
 
 
 
