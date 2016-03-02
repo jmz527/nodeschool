f4a844310aca79969f9b6fc676e8dd78b4ad24a7
James Rutledge <jarutledge2@gmail.com>
2016-03-01 22:27:11 -0500

    tagged template strings

--- count-to-6/tagged-template-strings.js
+++ count-to-6/tagged-template-strings.js
@@ -1,17 +1,38 @@
 // console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
 
-// function html(['<b>', ' says</b>: "', '"'], username, comment) {
-// 	username = username.replace("'", "&apos;");
-// 	username = username.replace('"', "&quot;");
-// 	username = username.replace('<', "&lt;");
-// 	username = username.replace('>', "&gt;");
-// 	username = username.replace('&', "&amp;");
+// function html(arry, username, comment) {
+
+// 	comment = comment.replace('&', "&amp;");
 // 	comment = comment.replace("'", "&apos;");
 // 	comment = comment.replace('"', "&quot;");
 // 	comment = comment.replace('<', "&lt;");
 // 	comment = comment.replace('>', "&gt;");
-// 	comment = comment.replace('&', "&amp;");
+
+// 	var output = arry.join(username);
+// 	output = output.replace(username+'"', comment+'"');
+
+// 	return output
 // }
 
-console.log(process.argv[2]);
-console.log(process.argv[3]);
+
+
+// OFFICIAL SOLUTION
+//_______________________________________________________________________________//
+console.log(html`<b>${process.argv[2]} says</b>: "${process.argv[3]}"`);
+
+function html(pieces, ...substitutions) {
+	var result = pieces[0];
+	for (var i = 0; i < substitutions.length; ++i) {
+		result += escape(substitutions[i]) + pieces[i + 1];
+	}
+
+	return result;
+}
+
+function escape(s) {
+	return s.replace(/&/g, "&amp;")
+			.replace(/</g, "&lt;")
+			.replace(/>/g, "&gt;")
+			.replace(/'/g, "&apos;")
+			.replace(/"/g, "&quot;");
+}
