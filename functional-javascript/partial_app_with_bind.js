27dbbdcc3b30a88faf0cc640d76821161f92df33
James Rutledge <jarutledge2@gmail.com>
2016-08-18 18:40:27 -0400

    ...

--- functional-javascript/partial_app_with_bind.js
+++ functional-javascript/partial_app_with_bind.js
@@ -1,6 +1,29 @@
+// var info = logger('INFO:')
+// info('this is an info message')
+// INFO: this is an info message
+
+// var warn = logger('WARN:')
+// warn('this is a warning message', 'with more info')
+// WARN: this is a warning message with more info
+
+
 // MY ANSWER
 //_______________________________________//
+module.exports = function(namespace) {
+	function logger(...args) {
+		var args = [namespace].concat(Array.prototype.slice.call(arguments)).join(' ');
+
+		return console.log(args);
+	}
+
+	return logger.bind(namespace);
+}
+
 
 
 // OFFICIAL ANSWER
 //_______________________________________//
+module.exports = function(namespace) {
+  return console.log.bind(console, namespace)
+}
+
