16332b337bde00d5462df4f1fc99145cf18ce091
James Rutledge <jarutledge2@gmail.com>
2016-08-22 16:29:17 -0400

    two more down

--- functional-javascript/function_spies.js
+++ functional-javascript/function_spies.js
@@ -1,6 +1,40 @@
+// var spy = Spy(console, 'error')
+
+// console.error('calling console.error')
+// console.error('calling console.error')
+// console.error('calling console.error')
+
+// console.log(spy.count) // 3
+
+
 // MY ANSWER
 //_______________________________________//
+function Spy(target, method) {
+	var oldFn, spy = {count: 0};
+		oldFn = target[method];
 
+	target[method] = function() {
+		spy.count++;
+		return oldFn.apply(target, arguments);
+	}
+	return spy;
+}
+
+module.exports = Spy;
 
 // OFFICIAL ANSWER
 //_______________________________________//
+// function Spy(target, method) {
+//   var originalFunction = target[method]
+
+//   // use an object so we can pass by reference, not value
+//   // i.e. we can return result, but update count from this scope
+//   var result = {
+//     count: 0
+//   }
+
+//   // replace method with spy method
+//   target[method] = function() {
+//     result.count++ // track function was called
+//     return originalFunction.apply(this, arguments) // invoke original function
+//   }
--- functional-javascript/map_with_reduce.js
+++ functional-javascript/map_with_reduce.js
@@ -1,6 +1,28 @@
+// var nums = [1,2,3,4,5]
+
+// // `map` is your exported function
+// var output = map(nums, function double(item) {
+//   return item * 2
+// })
+
+// console.log(output) // => [2,4,6,8,10]
+
+
 // MY ANSWER
 //_______________________________________//
+//  module.exports = function arrayMap(arr, fn) {
+//  	var newArr;
+// 	 	newArr = arr.map(fn);
+
+// 	return newArr
+// }
 
 
 // OFFICIAL ANSWER
 //_______________________________________//
+module.exports = function arrayMap(arr, fn, thisArg) {
+  return arr.reduce(function(acc, item, index, arr) {
+    acc.push(fn.call(thisArg, item, index, arr))
+    return acc
+  }, [])
+}
